67214a00b2d63ee411efcc7dea9edab912346b5a
Deepsh123 <105092065+Deepsh123@users.noreply.github.com>
2023-03-17 16:37:57 -0600

    Add files via upload

---
+++ function.js
@@ -0,0 +1,38 @@
+var zip_code = document.getElementById("zip_code").value;
+
+
+function display() {
+    zip_code = document.getElementById("zip_code").value;
+    if (zip_code == "80602"){
+        document.getElementById("1").innerHTML = "King Soopers";
+        document.getElementById("2").innerHTML = "Safeway";
+        document.getElementById("3").innerHTML = "Target";
+        document.getElementById("4").innerHTML = "Spice Bazaar";
+        document.getElementById("5").innerHTML = "Sprouts Farmer's Market";
+        document.getElementById("6").innerHTML = "Walmart";
+        document.getElementById("7").innerHTML = "Go Fresh";
+        document.getElementById("8").innerHTML = "Save a Lot";
+        document.getElementById("9").innerHTML = "Kohls";
+        document.getElementById("10").innerHTML = "Marshalls";
+        document.getElementById("11").innerHTML = "T.J Max";
+        document.getElementById("12").innerHTML = "Macy's";
+        document.getElementById("13").innerHTML = "JCPenny";
+        document.getElementById("14").innerHTML = "7-Eleven";
+        document.getElementById("15").innerHTML = "Circle-K";
+        document.getElementById("16").innerHTML = "Kum&Go";
+        document.getElementById("17").innerHTML = "Murphy";
+        document.getElementById("18").innerHTML = "Rocket";
+        document.getElementById("list").style.display = "";
+        document.getElementById("supermarkets").style.display = "";
+        document.getElementById("stores").style.display = "";
+        document.getElementById("list_1").style.display = "";
+        document.getElementById("store").style.display = "";
+        document.getElementById("list_2").style.display = "";
+        document.getElementById("19").innerHTML = "Ross";
+        document.getElementById("20").innerHTML = "Chico's";
+        document.getElementById("21").innerHTML = "Sears";
+        document.getElementById("##").style.display = "";
+        document.getElementById("#").style.display = "";
+        document.getElementById("###").style.display = "";
+    }
+}
